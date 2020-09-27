1315f1bb297495bc8bc83b099cff5e5248cf4fde
unknown <mahnoorarif5587@gmail.com>
2020-09-27 17:22:31 +0500

    Updates

---
+++ precache-manifest.56ef4f46b7a137c6e54e21fd784eb2d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f357b7ef96d457b677e9ee7ae630870",
+    "url": "/monster-Rolodex/index.html"
+  },
+  {
+    "revision": "1ffc9b331eaee419aff3",
+    "url": "/monster-Rolodex/static/css/main.4d3cff53.chunk.css"
+  },
+  {
+    "revision": "30ff922f61e75086b083",
+    "url": "/monster-Rolodex/static/js/2.ff504122.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-Rolodex/static/js/2.ff504122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffc9b331eaee419aff3",
+    "url": "/monster-Rolodex/static/js/main.842027fd.chunk.js"
+  },
+  {
+    "revision": "03983f285549b2c26115",
+    "url": "/monster-Rolodex/static/js/runtime-main.4ee002bb.js"
+  }
+]);
